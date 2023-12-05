1db086bb775fcbc3b62a8c5d895abf551b7fb9c0
AlexOrioon <alex02alex87@gmail.com>
2023-12-04 23:17:57 -0600

    Agregar DataContext

---
+++ Sales.API/Controllers/CountriesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sales.API.Data;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Controllers
+{
+    [ApiController]
+    [Route("api/countries")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post(Country country)
+        {
+            _context.Countries.Add(country);
+
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+    }
+}
